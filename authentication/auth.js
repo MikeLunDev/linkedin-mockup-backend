58c39fcea3a5d2b58275e129e2b8b34de19fe9a9
Michele Lunati <lunatimichele@gmail.com>
2019-10-28 00:22:50 +0100

    .

--- authentication/auth.js
+++ authentication/auth.js
@@ -10,7 +10,7 @@ passport.use(User.createStrategy());
 
 var options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: process.env.SECRET_KEY
+  secretOrKey: "thisisasupersecretkey"
 };
 passport.use(
   new JwtStrategy(options, (jwt_payload, done) => {
