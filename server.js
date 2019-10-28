bdb2ae07c4301c2ec6cd1d51ffe68155a89fe3fd
Michele Lunati <lunatimichele@gmail.com>
2019-10-28 09:03:19 +0100

    fixing deployment error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const passport = require("passport");
 const userRouter = require("./services/user");
 
 const server = express();
-server.set("port", process.env.PORT || 3005);
+server.set("port", 3005);
 server.use(express.json());
 server.use(passport.initialize());
 server.use("/poster", express.static(join(__dirname, "./public/imgs")));
@@ -17,13 +17,6 @@ server.use("/profile", cors(), profileRouter);
 server.use("/post", cors(), postRouter);
 server.use("/user", cors(), userRouter);
 console.log(listRoutes(server));
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
 
 mongoose
   .connect(
