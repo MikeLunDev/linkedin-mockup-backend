1763a3b50d20b410d1fb05ff6f7d9b9c8e7e0983
Michele Lunati <lunatimichele@gmail.com>
2019-10-28 08:31:38 +0100

    i'm desperate

--- authentication/auth.js
+++ authentication/auth.js
@@ -5,14 +5,14 @@ const ExtractJwt = require("passport-jwt").ExtractJwt;
 const LocalStrategy = require("passport-local").Strategy;
 const jwt = require("jsonwebtoken");
 
-passport.serializeUser(User.serializeUser()); //user.serial is from passport-local-mongoose
-passport.deserializeUser(User.deserializeUser());
-passport.use(new LocalStrategy(User.authenticate()));
-
 var options = {
   secretOrKey: "thisisasupersecretkey",
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
 };
+
+passport.serializeUser(User.serializeUser()); //user.serial is from passport-local-mongoose
+passport.deserializeUser(User.deserializeUser());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.use(
   new JwtStrategy(options, (jwt_payload, done) => {
