edcc093395f55e4b825763b4b022bfc2307beef1
Michele Lunati <lunatimichele@gmail.com>
2019-10-28 08:20:22 +0100

    s

--- authentication/auth.js
+++ authentication/auth.js
@@ -8,18 +8,13 @@ passport.serializeUser(User.serializeUser()); //user.serial is from passport-loc
 passport.deserializeUser(User.deserializeUser());
 passport.use(User.createStrategy());
 
-/* var options = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: "thisisasupersecretkey"
-}; */
+var options = {
+  secretOrKey: "thisisasupersecretkey",
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+};
 
-const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = "thisisasupersecretkey";
-opts.issuer = "accounts.examplesoft.com";
-opts.audience = "yoursite.net";
 passport.use(
-  new JwtStrategy(opts, (jwt_payload, done) => {
+  new JwtStrategy(options, (jwt_payload, done) => {
     //jwt_pay is the extracted json
     console.log("payload", jwt_payload);
     User.findById(jwt_payload._id, (err, user) => {
